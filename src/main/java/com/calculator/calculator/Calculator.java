c2d9aec5da9e29f31917fc84ae353d10c420a187
Timur-Gus <timurka24478@gmail.com>
2023-12-22 09:48:36 +0300

    Исправил

--- src/main/java/com/calculator/calculator/Calculator.java
+++ src/main/java/com/calculator/calculator/Calculator.java
@@ -11,40 +11,22 @@ public class Calculator implements CalculatorService  {
     }
 
     @Override
-    public String calculatorPlus(String num1, String num2) {
-        if (num1 == null || num2 == null) {
-            return "Ошибка! Вы не ввели одно или оба значения!";
-        }
-        int sum = Integer.valueOf(num1) + Integer.valueOf(num2);
-        return num1 + " + " + num2 + " = " + sum;
+    public int calculatorPlus(int num1, int num2) {
+        return num1 + num2;
 
     }
     @Override
-    public String calculatorMinus(String num1, String num2) {
-        if (num1 == null || num2 == null) {
-            return "Ошибка! Вы не ввели одно или оба значения!";
-        }
-        int diff = Integer.valueOf(num1) - Integer.valueOf(num2);
-        return num1 + " - " + num2 + " = " + diff;
+    public int calculatorMinus(int num1, int num2) {
+        return num1 - num2;
 
     }
     @Override
-    public String calculatorMultiply(String num1, String num2) {
-        if (num1 == null || num2 == null) {
-            return "Ошибка! Вы не ввели одно или оба значения!";
-        }
-        int mult = Integer.valueOf(num1) * Integer.valueOf(num2);
-        return num1 + " * " + num2 + " = " + mult;
+    public int calculatorMultiply(int num1, int num2) {
+        return num1 * num2;
     }
     @Override
-    public String calculatorDivide(String num1, String num2) {
-        if (num1 == null || num2 == null) {
-            return "Ошибка! Вы не ввели одно или оба значения!";
-        }
-        if (Integer.valueOf(num2) == 0) {
-            return "Деление на ноль невозможно";
-        }
-        int div = Integer.valueOf(num1) / Integer.valueOf(num2);
-        return num1 + " / " + num2 + " = " + div;
+    public int calculatorDivide(int num1, int num2) {
+        return num1 / num2;
     }
+
 }
--- src/main/java/com/calculator/calculator/CalculatorController.java
+++ src/main/java/com/calculator/calculator/CalculatorController.java
@@ -1,6 +1,8 @@
 package com.calculator.calculator;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,19 +19,22 @@ private final Calculator calculator;
     }
 
     @GetMapping(path = "/plus")
-    public String calculatorPlus(String num1, String num2) {
-        return calculator.calculatorPlus(num1, num2);
+    public String calculatorPlus(@RequestParam int num1,@RequestParam int num2) {
+        return num1 + " + " + num2 + " = " + calculator.calculatorPlus(num1, num2);
     }
     @GetMapping(path = "/minus")
-    public String calculatorMinus(String num1, String num2) {
-        return calculator.calculatorMinus(num1, num2);
+    public String calculatorMinus(@RequestParam int num1,@RequestParam int num2) {
+        return num1 + " - " + num2 + " = " + calculator.calculatorMinus(num1, num2);
     }
     @GetMapping(path = "/multiply")
-    public String calculatorMultiply(String num1, String num2) {
-        return calculator.calculatorMultiply(num1, num2);
+    public String calculatorMultiply(@RequestParam int num1,@RequestParam int num2) {
+        return num1 + " * " + num2 + " = " + calculator.calculatorMultiply(num1, num2);
     }
     @GetMapping(path = "/divide")
-    public String calculatorDivide(String num1, String num2) {
-        return calculator.calculatorDivide(num1, num2);
+    public String calculatorDivide(@RequestParam int num1,@RequestParam int num2) {
+        if (num2 == 0) {
+            return "Деление на ноль невозможно";
+        }
+        return num1 + " / " + num2 +  " = " + calculator.calculatorDivide(num1, num2);
     }
 }
